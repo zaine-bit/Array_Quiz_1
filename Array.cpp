eb80916b42cc5ae98ba01a1895f5197b9d29a863
zaine-bit <zainezira@gmail.com>
2026-03-02 23:35:24 +0800

    My quiz answers

--- Array.cpp
+++ Array.cpp
@@ -24,17 +24,36 @@ bool Array::Add(int value) {
 
 // Insert value at specified index, shifting elements to the right
 bool Array::InsertAt(int index, int value) {
-    return false;
+    if (size_ == capacity_ || index < 0 || index > size_) {
+        return false;
+    }
+    for (int i = size_; i > index; i--) {
+        data_[i] = data_[i - 1];
+    }
+    data_[index] = value;
+    size_++;
+    return true;
 }
 
 // Delete element at index, shifting elements left
 bool Array::DeleteAt(int index) {
-    return false;
+     if (index < 0 || index >= size_) {
+        return false;
+    }
+    for (int i = index; i < size_ - 1; i++) {
+        data_[i] = data_[i + 1];
+    }
+    size_--;
+    return true;
 }
 
 // Update element at index
 bool Array::UpdateAt(int index, int new_value) {
-    return false;
+    if (index < 0 || index >= size_) {
+        return false;
+    }
+    data_[index] = new_value;
+    return true;
 }
 
 // Print all elements
